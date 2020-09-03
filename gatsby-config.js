32e01ed80eb33c7b396e9470cffc9e670dbab91a
Halvor Bø <hfb@complex.codes>
2020-09-03 14:59:55 +0200

    Updated blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Complex Codes`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Halvor Fladsrud Bø`,
+      summary: `who lives and works in Oslo building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
